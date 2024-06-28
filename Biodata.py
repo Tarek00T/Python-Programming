0b63208806ceef99f62e9776bd8e871349eb43a1
Md Tarek Miah <140520331+Tarek00T@users.noreply.github.com>
2024-06-28 10:47:45 +0600

    Add files via upload

---
+++ Biodata.py
@@ -0,0 +1,18 @@
+
+# Information about Md Tarek Miah
+name = "Md Tarek Miah"
+student_id = "231-115-208"
+section = "F"
+house = "Habiganj (Olipur)"
+age = 23
+marital_status = "unmarried"
+favorite_languages = ["c", "c++", "java", "python", "c#", "php"]
+
+# Print the information
+print(f"Name: {name}")
+print(f"ID: {student_id}")
+print(f"Section: {section}")
+print(f"House: {house}")
+print(f"Age: {age} years old")
+print(f"Marital Status: {marital_status}")
+print(f"Favorite Languages: {', '.join(favorite_languages)}")
